4c1fc254e8f38f64a173450d37ccca57011f7262
haileyhansard <haileyhansard@gmail.com>
2020-05-19 18:09:57 -0700

    Finished Task #1

--- index.js
+++ index.js
@@ -12,14 +12,27 @@ console.log('its working');
 (d) Away Team goals for 2014 world cup final
 (e) Winner of 2014 world cup final */
 
+//You should use a filter to get the object and then bracket notation to retrieve the info
+
+
+let result = fifaData.filter(function(element){
+    return element.Year === 2014 && element.Stage === 'Final';
+});
+console.log(result[0]['Home Team Name']);
+console.log(result[0]['Away Team Name']);
+console.log(result[0]['Home Team Goals']);
+console.log(result[0]['Away Team Goals']);
+console.log(result[0]['Win conditions']);
+
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
+// function getFinals(data) {
+// const finalsData = data.filter(function(element){
+//     return element.Stage === "Final";
+// });
+//  console.log(getFinals(finalsData)); //??
 
-    /* code here */
-
-};
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
